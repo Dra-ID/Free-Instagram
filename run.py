5c776895d8579ad74639cd44f505d01a22931ec0
vindra XD <104926748+Dra-ID@users.noreply.github.com>
2023-07-26 14:04:28 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,2 +1,2 @@
-print('''\nSc sudah di hapus
+print('''\nUtamakan Baca Kalo bisa baca Kalo kaga bisa baca ya tolol\nSc sudah di hapus
 gak terima kalo di hapus bacotin nih https://t.me/Vindra_ID''')
